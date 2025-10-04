569f4f89d852feca95195c81fb323822a87a5778
Lin <lin@Lins-MacBook-Air.local>
2025-10-04 23:57:36 +0200

    start from scratch

--- frontend/src/hooks/index.js
+++
@@ -1,9 +0,0 @@
-// hooks/index.js
-export { useAuth, useGitHubAuth } from './useAuth';
-export { useRepository } from './useRepository';
-export { useDocumentation } from './useDocumentation';
-export { useAnalysis } from './useAnalysis';
-export { useWebSocket } from './useWebSocket';
-export { useLocalStorage } from './useLocalStorage';
-export { useDebounce } from './useDebounce';
-export { useApi } from './useApi';
